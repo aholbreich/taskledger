73c9cff6e457fe5155b83bcdacab13a8cf511885
Alexander Holbreich <alexander@holbreich.org>
2026-05-23 16:49:13 +0200

    fix: polishing Formula

--- Formula/tl.rb
+++ Formula/tl.rb
@@ -10,24 +10,24 @@ class Tl < Formula
   desc "Git-native task ledger for human and AI agent coordination"
   homepage "https://github.com/aholbreich/tl"
   license "MIT"
-  version "0.4.0"
+  version "0.4.1"
 
   # --- Platform-specific binary archives (stable release) ---
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/aholbreich/tl/releases/download/v#{version}/tl-darwin-amd64.tar.gz"
+      url "https://github.com/aholbreich/tl/releases/download/#{version}/tl-darwin-amd64.tar.gz"
       sha256 "REPLACE_WITH_DARWIN_AMD64_SHA256"
     else
-      url "https://github.com/aholbreich/tl/releases/download/v#{version}/tl-darwin-arm64.tar.gz"
+      url "https://github.com/aholbreich/tl/releases/download/#{version}/tl-darwin-arm64.tar.gz"
       sha256 "REPLACE_WITH_DARWIN_ARM64_SHA256"
     end
   end
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/aholbreich/tl/releases/download/v#{version}/tl-linux-amd64.tar.gz"
+      url "https://github.com/aholbreich/tl/releases/download/#{version}/tl-linux-amd64.tar.gz"
       sha256 "REPLACE_WITH_LINUX_AMD64_SHA256"
     else
-      url "https://github.com/aholbreich/tl/releases/download/v#{version}/tl-linux-arm64.tar.gz"
+      url "https://github.com/aholbreich/tl/releases/download/#{version}/tl-linux-arm64.tar.gz"
       sha256 "REPLACE_WITH_LINUX_ARM64_SHA256"
     end
   end
